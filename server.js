39b739bd568ac221784b49ebba371c0dff5563f4
ChinmayaGarg <chinmaya.garg1997@gmail.com>
2023-04-16 16:13:54 +0530

    Learning: Why NodeJS is a event driven architecture?

--- server.js
+++ server.js
@@ -25,4 +25,9 @@ The function passed as an arguement to createServer takes in 2 parameters as des
 Another way to pass a requestListener is by using annonymous function.
 
 requestListener is a callback function because it is called by nodejs whenever a request reaches our server.
+
+* Event Driven Architecture:
+The node will execute this requestListener function, passed as arguement in http.createServer, whenever a request reasches our server.
+This is a event-driven architecture, that nodejs uses heavily.
+We work a lot with such setups or such code snippets where we tell node if X happens, do Y, as in above case if request comes please execute requestListener. 
 */
